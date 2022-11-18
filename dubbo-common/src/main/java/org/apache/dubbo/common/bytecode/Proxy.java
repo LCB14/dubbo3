588f103c740d7c7f6ad968ee5f2a3d6bb574c3da
lichangbao <lichangbao@moqipobing.com>
2022-11-18 10:59:02 +0800

    2022年11月18日10:58:56

--- dubbo-common/src/main/java/org/apache/dubbo/common/bytecode/Proxy.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/bytecode/Proxy.java
@@ -216,28 +216,55 @@ public abstract class Proxy {
             ccp.addDefaultConstructor();
 
             /**
+             * Arthas 反编译步骤：
+             * 1. 启动 Arthas
+             *    java -jar arthas-boot.jar
+             *
+             * 2. 输入编号选择进程
+             *    Arthas 启动后，会打印 Java 应用进程列表，如下：
+             *    [1]: 11232 org.jetbrains.jps.cmdline.Launcher
+             *    [2]: 22370 org.jetbrains.jps.cmdline.Launcher
+             *    [3]: 22371 com.alibaba.dubbo.demo.consumer.Consumer
+             *    [4]: 22362 com.alibaba.dubbo.demo.provider.Provider
+             *    [5]: 2074 org.apache.zookeeper.server.quorum.QuorumPeerMain
+             * 这里输入编号 3，让 Arthas 关联到启动类为 com.....Consumer 的 Java 进程上
+             *
+             * 3. 由于 Demo 项目中只有一个服务接口，因此此接口的代理类类名为 proxy0，此时使用 sc 命令搜索这个类名。
+             *    $ sc *.proxy0
+             *    com.alibaba.dubbo.common.bytecode.proxy0
+             *
+             * 4. 使用 jad 命令反编译 com.alibaba.dubbo.common.bytecode.proxy0
+             *    $ jad com.alibaba.dubbo.common.bytecode.proxy0
+             *
+             * 更多使用方法请参考 Arthas 官方文档：https://arthas.aliyun.com/doc/quick-start.html
+             *
              * ccp 对应代理代码参考：
+             * public class proxy0 implements ClassGenerator.DC, EchoService, DemoService {
+             *     // 方法数组
+             *     public static Method[] methods;
+             *     private InvocationHandler handler;
              *
-             * package org.apache.dubbo.common.bytecode;
-             *
-             * public class proxy0 implements org.apache.dubbo.demo.DemoService {
-             *
-             *     public static java.lang.reflect.Method[] methods;
-             *
-             *     private java.lang.reflect.InvocationHandler handler;
+             *     public proxy0(InvocationHandler invocationHandler) {
+             *         this.handler = invocationHandler;
+             *     }
              *
              *     public proxy0() {
              *     }
              *
-             *     public proxy0(java.lang.reflect.InvocationHandler arg0) {
-             *         handler = $1;
+             *     public String sayHello(String string) {
+             *         // 将参数存储到 Object 数组中
+             *         Object[] arrobject = new Object[]{string};
+             *         // 调用 InvocationHandler 实现类的 invoke 方法得到调用结果
+             *         Object object = this.handler.invoke(this, methods[0], arrobject);
+             *         // 返回调用结果
+             *         return (String)object;
              *     }
              *
-             *     public java.lang.String sayHello(java.lang.String arg0) {
-             *         Object[] args = new Object[1];
-             *         args[0] = ($w) $1;
-             *         Object ret = handler.invoke(this, methods[0], args);
-             *         return (java.lang.String) ret;
+             *     // 回声测试方法
+             *     public Object $echo(Object object) {
+             *         Object[] arrobject = new Object[]{object};
+             *         Object object2 = this.handler.invoke(this, methods[1], arrobject);
+             *         return object2;
              *     }
              * }
              */
